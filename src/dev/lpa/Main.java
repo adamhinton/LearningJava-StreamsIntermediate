90b5e242f488e4e13481ce94d98327684ca448f1
adamd <adam.d.hinton@gmail.com>
2024-04-04 23:34:15 -0400

    Notes and filter examples

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        IntStream.iterate((int) 'A', i-> i <= (int) 'z', i -> i + 1)
+                .filter(Character::isAlphabetic)
+                .map(Character::toUpperCase)
+                .distinct()
+//                .filter(i -> Character.toUpperCase(i) > 'E')
+//                .skip(5)
+//                .takeWhile(i -> i < 'a')
+                // Only evaluated for els until it first becomes false
+                // Once it reaches 'F' it stops checking
+                // This distinguishes it in fxnlty from .filter
+//                .dropWhile(i -> Character.toUpperCase(i )<= 'E')
+                .forEach(d -> System.out.printf("%c ", d));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println("---------------------");
+        System.out.println("---------------------");
+
+        Random random = new Random();
+
+        // If this feels like SQL that's for good reason
+        // Lets you say what should happen, not *how* it will happen
+        // Lets us do what we're good at, in other words.
+        Stream.generate(() -> random.nextInt((int)'A', (int)'Z' + 1))
+                .limit(50)
+//                .distinct()
+                .sorted()
+                .forEach(d-> System.out.printf("%c", d));
+
     }
 }
